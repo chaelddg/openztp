baf8dc8b63676014a8b2ca55c5aeec5e0f8b2fb0
Chael Kales <chael.ddg@gmail.com>
2017-10-21 20:30:02 +0800

    added css/sass/less config

--- pages/index.js
+++ pages/index.js
@@ -1,44 +1,79 @@
 import React from 'react'
 import Link from 'next/link'
 import Head from 'next/head'
-import { Menu, Icon } from 'antd'
+
+import ss from './index.sass'
+
+import { Layout, Menu, Breadcrumb, Icon } from 'antd';
+const { Header, Content, Footer, Sider } = Layout;
 
 {/* <Link href='/a'><a>Home</a></Link> | */}
 
 const SubMenu = Menu.SubMenu;
 const MenuItemGroup = Menu.ItemGroup;
-
 export default ({ children }) => (
-	<div>
+	<Layout>
 		<Head>
-			<meta name='viewport' content='width=device-width, initial-scale=1' />
-			<meta charSet='utf-8' />
-			<link rel='stylesheet' href='//cdnjs.cloudflare.com/ajax/libs/antd/2.13.6/antd.min.css' />
-		</Head>
-		<Menu
-			onClick={()=>{}}
-			selectedKeys={['mail']}
-			mode="horizontal"
-		>
-			<Menu.Item key="mail">
-				<Icon type="mail" />Navigation One
-			</Menu.Item>
-			<Menu.Item key="app" disabled>
-				<Icon type="appstore" />Navigation Two
-			</Menu.Item>
-			<SubMenu title={<span><Icon type="setting" />Navigation Three - Submenu</span>}>
-				<MenuItemGroup title="Item 1">
-					<Menu.Item key="setting:1">Option 1</Menu.Item>
-					<Menu.Item key="setting:2">Option 2</Menu.Item>
-				</MenuItemGroup>
-				<MenuItemGroup title="Item 2">
-					<Menu.Item key="setting:3">Option 3</Menu.Item>
-					<Menu.Item key="setting:4">Option 4</Menu.Item>
-				</MenuItemGroup>
-			</SubMenu>
-			<Menu.Item key="alipay">
-				<a href="https://ant.design" target="_blank" rel="noopener noreferrer">Navigation Four - Link</a>
-			</Menu.Item>
-		</Menu>
-	</div>
+      <title>OpenZtp</title>
+      <meta charSet='utf-8' />
+      <meta name='viewport' content='initial-scale=1.0, width=device-width' />
+			<link rel="stylesheet" href="//cdnjs.cloudflare.com/ajax/libs/antd/2.13.6/antd.min.css"/>
+			<link rel='stylesheet' href='/static/css/bundle.css' />
+    </Head>
+    <Header className="header">
+      <div className={ss.logo}/>
+      <Menu
+        theme="dark"
+        mode="horizontal"
+        defaultSelectedKeys={['2']}
+        style={{ lineHeight: '64px' }}
+      >
+        <Menu.Item key="1">nav 1</Menu.Item>
+        <Menu.Item key="2">nav 2</Menu.Item>
+        <Menu.Item key="3">nav 3</Menu.Item>
+      </Menu>
+    </Header>
+    <Content style={{ padding: '0 50px' }}>
+      <Breadcrumb style={{ margin: '16px 0' }}>
+        <Breadcrumb.Item>Home</Breadcrumb.Item>
+        <Breadcrumb.Item>List</Breadcrumb.Item>
+        <Breadcrumb.Item>App</Breadcrumb.Item>
+      </Breadcrumb>
+      <Layout style={{ padding: '24px 0', background: '#fff' }}>
+        <Sider width={200} style={{ background: '#fff' }}>
+          <Menu
+            mode="inline"
+            defaultSelectedKeys={['1']}
+            defaultOpenKeys={['sub1']}
+            style={{ height: '100%' }}
+          >
+            <SubMenu key="sub1" title={<span><Icon type="user" />subnav 1</span>}>
+              <Menu.Item key="1">option1</Menu.Item>
+              <Menu.Item key="2">option2</Menu.Item>
+              <Menu.Item key="3">option3</Menu.Item>
+              <Menu.Item key="4">option4</Menu.Item>
+            </SubMenu>
+            <SubMenu key="sub2" title={<span><Icon type="laptop" />subnav 2</span>}>
+              <Menu.Item key="5">option5</Menu.Item>
+              <Menu.Item key="6">option6</Menu.Item>
+              <Menu.Item key="7">option7</Menu.Item>
+              <Menu.Item key="8">option8</Menu.Item>
+            </SubMenu>
+            <SubMenu key="sub3" title={<span><Icon type="notification" />subnav 3</span>}>
+              <Menu.Item key="9">option9</Menu.Item>
+              <Menu.Item key="10">option10</Menu.Item>
+              <Menu.Item key="11">option11</Menu.Item>
+              <Menu.Item key="12">option12</Menu.Item>
+            </SubMenu>
+          </Menu>
+        </Sider>
+        <Content style={{ padding: '0 24px', minHeight: 280 }}>
+          Content
+        </Content>
+      </Layout>
+    </Content>
+    <Footer style={{ textAlign: 'center' }}>
+      Ant Design ©2016 Created by Ant UED
+    </Footer>
+  </Layout>
 )
